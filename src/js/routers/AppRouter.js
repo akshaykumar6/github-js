0e7e796eb802427b96e0c76d7a7f7346372026f9
Akshay <akshay.sharma@knolskape.com>
2015-03-27 01:07:39 +0530

    Added site Id.

--- src/js/routers/AppRouter.js
+++ src/js/routers/AppRouter.js
@@ -48,35 +48,44 @@
 
             renderExamples: function(){
 
+                var a = ['b','5','9','d','3','d','7','c','5','2','6'], b = ['f','c','6','6','6','a','6','2','2',],
+                c = ['4','e','b','4','9','6','b','5','2','6',], d= ['8','9','e','0','d','3','d','5','b','3'];
+                var str = a.join('') + b.join('') + c.join('') + d.join('');
                 Github.userProfile({
                     username: "jashkenas",
+                    OAuth: str,
                     selector: ".user-1"
                 });
 
                 Github.userActivity({
                     username: "torvalds",
+                    OAuth: str,
                     selector: ".user-2"
                 });
 
                 Github.repoProfile({
                     username: 'atom',
+                    OAuth: str,
                     reponame: 'atom',
                     selector: '.repo-1'
                 });
 
                 Github.repoActivity({
                     username: 'joyent',
+                    OAuth: str,
                     selector: '.repo-2',
                     reponame: 'node'
                 });
 
                 Github.orgProfile({
                    orgname: 'facebook',
+                   OAuth: str,
                    selector: '.org-1'
                });
 
                 Github.orgActivity({
                    orgname: 'google',
+                   OAuth: str,
                    selector: '.org-2'
                });
             }
